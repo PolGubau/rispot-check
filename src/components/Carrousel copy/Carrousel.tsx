ce097d07a6ab47f49818024f330d92bbb6f68f32
Pol Gubau <gubaupol@gmail.com>
2022-09-16 13:17:45 +0200

    Create Carrousel.tsx

---
+++ src/components/Carrousel copy/Carrousel.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "./Carrousel.module.css";
+export default function Carrousel() {
+  return (
+    <>
+      <p>Productos en oferta 🔥</p>
+      <div className={styles.carrousel}>carrousel</div>
+    </>
+  );
+}
