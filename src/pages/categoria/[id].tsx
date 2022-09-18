9a74d179e67371e1bab26fee54fca498bcdea10d
Pol Gubau <gubaupol@gmail.com>
2022-09-18 19:32:03 +0200

    Update [id].tsx

--- src/pages/categoria/[id].tsx
+++ src/pages/categoria/[id].tsx
@@ -1,12 +1,22 @@
 import { useRouter } from "next/router";
-
+import { Sales } from "src/utils/Data";
+import { Sale } from "src/interfaces/tableInterfaces";
+import SaleGrid from "src/components/Grids/SaleGrid/SaleGrid";
 export default function Categoria() {
   const { query } = useRouter();
-  console.log(query);
+  const categorySearched = query.id;
+
+  //filter which sales are in the category
+  const sales: Sale[] = Sales.filter(
+    (sale) => sale.category === categorySearched
+  );
+
   return (
     <div>
       <h1>Categoria</h1>
       <p>{query.id}</p>
+      <p>{sales.length}</p>
+      <SaleGrid array={sales} />
     </div>
   );
 }
