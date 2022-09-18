f013d3d61024ee3c00bb1c06f8459174a74a8a4f
Pol Gubau <gubaupol@gmail.com>
2022-09-18 20:49:23 +0200

    Update [id].tsx

--- src/pages/categoria/[id].tsx
+++ src/pages/categoria/[id].tsx
@@ -2,6 +2,7 @@ import { useRouter } from "next/router";
 import { Sales } from "src/utils/Data";
 import { Sale } from "src/interfaces/tableInterfaces";
 import SaleGrid from "src/components/Grids/SaleGrid/SaleGrid";
+import styles from "./category.module.css";
 export default function Categoria() {
   const { query } = useRouter();
   const categorySearched = query.id;
@@ -12,11 +13,11 @@ export default function Categoria() {
   );
 
   return (
-    <div>
+    <main className={styles.main}>
       <h1>Categoria</h1>
       <p>{query.id}</p>
       <p>{sales.length}</p>
       <SaleGrid array={sales} />
-    </div>
+    </main>
   );
 }
