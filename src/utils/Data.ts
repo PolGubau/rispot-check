ff11821bbaf758797851d06a8a7eac2361ea4804
Pol Gubau <gubaupol@gmail.com>
2022-09-22 11:03:26 +0200

    Update Data.ts

--- src/utils/Data.ts
+++ src/utils/Data.ts
@@ -4,7 +4,6 @@ export const Sales: Sale[] = [
   {
     id: 1,
     name: "Mejores manzanas del mercado",
-    description: "Fruits Desripcion",
     oldPrice: 100,
     newPrice: 50,
     image: "https://picsum.photos/200/300",
@@ -12,11 +11,11 @@ export const Sales: Sale[] = [
     category: "Frutas",
     stars: 48,
     valorations: 18,
+    link: "",
   },
   {
     id: 2,
     name: "Nuevas frutas asdasdasddaasdasdasddasdasdaasdasdasddasdasdasdasda para ver",
-    description: "Fruits Desripcion",
     oldPrice: 75,
     newPrice: 80,
     image: "https://m.media-amazon.com/images/I/51JgH9mik9L._AC_SY135_.jpg",
@@ -24,11 +23,11 @@ export const Sales: Sale[] = [
     category: "Frutas",
     stars: 25,
     valorations: 10,
+    link: "",
   },
   {
     id: 3,
     name: "Mejor manzanas del mercado",
-    description: "Fruits Desripcion",
     oldPrice: 100,
     newPrice: 50,
     image: "https://picsum.photos/200/300",
@@ -36,11 +35,11 @@ export const Sales: Sale[] = [
     category: "Frutas",
     stars: 48,
     valorations: 10,
+    link: "",
   },
   {
     id: 4,
     name: "Nuevas frutas para ver",
-    description: "Fruits Desripcion",
     oldPrice: 75,
     newPrice: 80,
     image: "https://m.media-amazon.com/images/I/61hcR7g-0WL._AC_SX679_.jpg",
@@ -48,6 +47,7 @@ export const Sales: Sale[] = [
     category: "Frutas",
     stars: 48,
     valorations: 10,
+    link: "",
   },
 ];
 
